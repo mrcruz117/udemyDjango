fc52e8f78459e070f797ca002c002e8fd6c6f768
mrcruz117 <mrcruz1989@gmail.com>
2021-12-21 17:19:00 -0500

    set up new models

--- demo/models.py
+++ demo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Book(models.Model):
+    BOOKS = (
+        ('HB', 'Hobbit'),
+        ('LOTR', 'Lord of the rings')
+    )
+    title = models.CharField(blank=True)
