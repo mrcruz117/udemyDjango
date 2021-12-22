8dae81d75b9dbee41dd61522ce2393dfb565499f
mrcruz117 <mrcruz1989@gmail.com>
2021-12-22 15:16:44 -0500

    Displaying book titles through template loop

--- demo/urls.py
+++ demo/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from .views import Class_View_One
 
 urlpatterns = [
 
     path('first', views.first),
-    path('another', Class_View_One.as_view())
 ]
--- demo/views.py
+++ demo/views.py
@@ -2,18 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 from .models import Book
+from django.shortcuts import render
 
 
 # Create your views here.
 
-class Class_View_One(View):
-    books = Book.objects.all()
-
-    output = f"We have {len(books)} books in the DB."
-
-    def get(self, request):
-        return HttpResponse(self.output)
-
 
 def first(request):
-    return HttpResponse('First message from views')
+    books = Book.objects.all()
+
+    return render(request, 'first_temp.html', {"books": books})
