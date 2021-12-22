c425aa033de79f1c5d8bd9bc57fa203a0cf303a6
mrcruz117 <mrcruz1989@gmail.com>
2021-12-22 14:46:34 -0500

    template init

--- demo/urls.py
+++ demo/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from .views import Class_View_One
 
 urlpatterns = [
-    path('/', views.first),
 
+    path('first', views.first),
+    path('another', Class_View_One.as_view())
 ]
--- demo/views.py
+++ demo/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+from .models import Book
+
+
 # Create your views here.
+
+class Class_View_One(View):
+    books = Book.objects.all()
+
+    output = f"We have {len(books)} books in the DB."
+
+    def get(self, request):
+        return HttpResponse(self.output)
+
+
 def first(request):
     return HttpResponse('First message from views')
