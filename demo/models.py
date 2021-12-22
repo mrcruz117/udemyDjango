9ff87b907ea2e261ca4bde4c76591db1ea3f3789
mrcruz117 <mrcruz1989@gmail.com>
2021-12-22 11:09:21 -0500

    first view function and set up first url

--- demo/models.py
+++ demo/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    BOOKS = (
-        ('HB', 'Hobbit'),
-        ('LOTR', 'Lord of the rings')
-    )
-    title = models.CharField(blank=True)
+    title = models.CharField(max_length=36, unique=True, blank=False)
+    description = models.TextField(max_length=256, blank=True)
+    price = models.DecimalField(default=0, max_digits=4, decimal_places=2)
+    published = models.DateField(blank=True, null=True, default=None)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='covers/', blank=True)
