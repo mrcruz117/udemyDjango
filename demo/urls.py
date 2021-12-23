a5b70a287f397b90c0142b3f425f49fff7509abd
mrcruz117 <mrcruz1989@gmail.com>
2021-12-23 10:29:10 -0500

    rest framework methods are working

--- demo/urls.py
+++ demo/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework import routers
+from .views import BookViewSet
+
+router = routers.DefaultRouter()
+router.register('books', BookViewSet)
 
 urlpatterns = [
 
-    path('first', views.first),
+    path('', include(router.urls)),
 ]
--- demo/views.py
+++ demo/views.py
@@ -3,12 +3,19 @@ from django.http import HttpResponse
 from django.views import View
 from .models import Book
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import BookSerializer
 
 
 # Create your views here.
 
 
-def first(request):
-    books = Book.objects.all()
+# def first(request):
+#     books = Book.objects.all()
+#
+#     return render(request, 'first_temp.html', {"books": books})
 
-    return render(request, 'first_temp.html', {"books": books})
+
+class BookViewSet(viewsets.ModelViewSet):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
